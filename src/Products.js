a5145f60be72c406c7b3b8a27f9a003d73dca855
Miguel Murillo <dotmaike@MacBook-Pro-de-Miguel.local>
2017-08-29 19:06:02 -0500

    Load products from firebase

--- src/Products.js
+++ src/Products.js
@@ -4,15 +4,7 @@ import ProductTable from './ProductTable.js';
 import ProductForm from './ProductForm';
 //import ProductRow from './ProductRow';
 import clone from 'lodash/clone';
-
-var PRODUCTS = {
-  '1': {id: 1, category: 'Musical Instruments', price: '$459.99', stocked: true, name: 'Clarinet', date:'01-01-2000'},
-  '2': {id: 2, category: 'Musical Instruments', price: '$5,000', stocked: true, name: 'Harpsicord' , date:'01-01-2000'},
-  '3': {id: 3, category: 'Musical Instruments', price: '$11,000', stocked: false, name: 'Fortepiano', date:'01-01-2000'},
-  '4': {id: 4, category: 'Furniture', price: '$799', stocked: true, name: 'Chaise Lounge', date:'01-01-2000'},
-  '5': {id: 5, category: 'Furniture', price: '$1,300', stocked: false, name: 'Dining Table', date:'01-01-2000'},
-  '6': {id: 6, category: 'Furniture', price: '$100', stocked: true, name: 'Bean Bag', date:'01-01-2000'}
-};
+import FB from './firebase.config';
 
 class Products extends Component {
   constructor(props) {
@@ -20,7 +12,7 @@ class Products extends Component {
     this.state = {
       filterText: '',
       inStockOnly: false,
-      products: PRODUCTS
+      products: {}
     };
 
     this.handleFilter = this.handleFilter.bind(this);
@@ -28,6 +20,14 @@ class Products extends Component {
     this.saveProduct = this.saveProduct.bind(this);
     this.editProduct = this.editProduct.bind(this);
     this.updateProduct = this.updateProduct.bind(this);
+  }
+
+  componentWillMount() {
+    const db  = FB.database();
+    const rootRef = db.ref().child('products');
+    rootRef.on('value', snap => {
+      this.setState(state => ({ ...state, products: snap.val() }));
+    });
   }
 
   handleFilter(filterInput) {
