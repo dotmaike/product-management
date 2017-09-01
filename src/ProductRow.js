cef07983fadd3a1bf8d9f71a3c1d320090597de6
Miguel Murillo <dotmaike@MacBook-Pro-de-Miguel.local>
2017-09-01 16:57:31 -0500

    Added fix for update - not complete

--- src/ProductRow.js
+++ src/ProductRow.js
@@ -1,67 +1,92 @@
-import React from 'react';
+import React from "react";
+import FB from './firebase.config';
 
 class ProductRow extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       isEditing: false,
       newName: props.product.name
     };
   }
 
   toggleState = () => {
-  	this.setState((pstate) => ({
-  		isEditing: !pstate.isEditing
-  	}))
-  }
+    this.setState(state => ({
+      ...state,
+      isEditing: !state.isEditing
+    }));
+  };
 
-  updateProduct = (e) => {
-    e.preventDefault();
-    this.props.editProduct(this.props.product.id, this.state.newName);
+  updateProduct = () => {
+    const { id } = this.props.product;
+    const { newName, newCategory, newPrice, newDate } = this.state;
+    const db = FB.database();
+    const rootRef = db.ref().child('products');
+    console.log(id.toString());
+    rootRef.child(id.toString())
+      .update({
+        name: newName,
+        category: newCategory,
+        price: newPrice,
+        date: newDate
+      })
+      .then(res => {
+        this.toggleState();
+        return {};
+      })
+      .catch(error => {
+        return {
+          errorCode: error.code,
+          errorMessage: error.message
+        };
+      });
+  };
 
-    this.setState({
-      isEditing: false
-    })
-  }
-
-  handleNameChange = (e) => {
+  handleNameChange = e => {
     this.setState({
       newName: e.target.value
-    })
-  }
+    });
+  };
 
   destroy = () => {
     this.props.onDestroy(this.props.product.id);
-  }
+  };
 
-  toCurrency = (price) => (price.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+  toCurrency = price =>
+    price.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 
   renderForm = () => (
-  	<form onSubmit={this.updateProduct}>
-      <input type="text" value={this.state.newName} onChange={this.handleNameChange} />
-          <input type="text" defaultValue={this.props.product.price} />
-          <input type="text" defaultValue={this.props.product.category} />
-          <input type="date" defaultValue={this.props.product.date} />
-      <button type="submit">Update</button>
-	  </form>
+    <form>
+      <input
+        type="text"
+        value={this.state.newName}
+        onChange={this.handleNameChange}
+      />
+      <input type="text" defaultValue={this.props.product.price} />
+      <input type="text" defaultValue={this.props.product.category} />
+      <input type="date" defaultValue={this.props.product.date} />
+      <button onClick={this.updateProduct} type="button">Update</button>
+    </form>
   );
 
   renderItem = () => (
-  	<tr>
-        <td>{this.props.product.name}</td>
-        <td>${this.toCurrency(this.props.product.price)}</td>
-        <td>{this.props.product.category}</td>
-        <td>{this.props.product.date}</td>
-        <td><button onClick={this.destroy}>Delete</button></td>
-        <td><button onClick={this.toggleState}>Edit</button></td>
+    <tr>
+      <td>{this.props.product.name}</td>
+      <td>${this.toCurrency(this.props.product.price)}</td>
+      <td>{this.props.product.category}</td>
+      <td>{this.props.product.date}</td>
+      <td>
+        <button onClick={this.destroy}>Delete</button>
+      </td>
+      <td>
+        <button onClick={this.toggleState}>Edit</button>
+      </td>
     </tr>
   );
 
   render() {
-    const {isEditing} = this.state;
-    return (
-     isEditing ? this.renderForm() : this.renderItem()
-    );
+    const { isEditing } = this.state;
+    return isEditing ? this.renderForm() : this.renderItem();
   }
 }
 
